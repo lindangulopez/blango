b6a55886c6f7666aacb95eb4cecad24547a6fb91
Linda López <angulo.linda@gmail.com>
2026-07-21 10:31:53 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,8 +1,15 @@
+import logging
+
 from django import template
 from django.contrib.auth.models import User
 from django.utils.html import format_html
 
+from blog.models import Post
+
+
 register = template.Library()
+
+logger = logging.getLogger(__name__)
 
 
 @register.filter
@@ -14,3 +21,16 @@ def author_details(author):
         return f"{author.first_name} {author.last_name}"
 
     return author.username
+
+
+@register.inclusion_tag("blog/recent_posts.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk).order_by("-published_at")[:5]
+
+    logger.debug(
+        "Loaded %d recent posts for post %d",
+        len(posts),
+        post.pk,
+    )
+
+    return {"posts": posts}
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 import logging
 
+from django.core.cache import cache
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 from blog.models import Post
 from blog.forms import CommentForm
@@ -11,54 +14,102 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+@cache_page(300)
+@vary_on_cookie
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    try:
+        posts = Post.objects.filter(published_at__lte=timezone.now())
 
-    logger.debug("Got %d posts", len(posts))
+        logger.debug("Got %d posts", len(posts))
 
-    return render(
-        request,
-        "blog/index.html",
-        {"posts": posts},
-    )
+        return render(
+            request,
+            "blog/index.html",
+            {"posts": posts},
+        )
+
+    except Exception as e:
+        logger.exception("Error loading index page: %s", e)
+
+        return render(
+            request,
+            "blog/index.html",
+            {"posts": []},
+        )
 
 
 def grid(request):
-    return render(request, "blog/grid.html")
+    try:
+        return render(request, "blog/grid.html")
+
+    except Exception as e:
+        logger.exception("Error loading grid page: %s", e)
+
+        return render(
+            request,
+            "blog/grid.html",
+            {},
+        )
 
 
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
+    try:
+        post = get_object_or_404(Post, slug=slug)
 
-    if request.user.is_active:
-        if request.method == "POST":
-            comment_form = CommentForm(request.POST)
+        if request.user.is_active:
 
-            if comment_form.is_valid():
-                comment = comment_form.save(commit=False)
-                comment.content_object = post
-                comment.creator = request.user
-                comment.save()
+            if request.method == "POST":
+                comment_form = CommentForm(request.POST)
 
-                logger.info(
-                    "Created comment on Post %d for user %s",
-                    post.pk,
-                    request.user,
-                )
+                if comment_form.is_valid():
+                    try:
+                        comment = comment_form.save(commit=False)
+                        comment.content_object = post
+                        comment.creator = request.user
+                        comment.save()
 
-                return redirect(request.path_info)
+                        logger.info(
+                            "Created comment on Post %d for user %s",
+                            post.pk,
+                            request.user,
+                        )
+
+                        return redirect(request.path_info)
+
+                    except Exception as e:
+                        logger.exception(
+                            "Error saving comment for post %d: %s",
+                            post.pk,
+                            e,
+                        )
+
+            else:
+                comment_form = CommentForm()
 
         else:
-            comment_form = CommentForm()
+            comment_form = None
 
-    else:
-        comment_form = None
+        return render(
+            request,
+            "blog/post-detail.html",
+            {
+                "post": post,
+                "comment_form": comment_form,
+            },
+        )
 
-    return render(
-        request,
-        "blog/post-detail.html",
-        {
-            "post": post,
-            "comment_form": comment_form,
-        },
-    )
+    except Exception as e:
+        logger.exception(
+            "Error loading post detail for slug %s: %s",
+            slug,
+            e,
+        )
+
+        return render(
+            request,
+            "blog/post-detail.html",
+            {
+                "post": None,
+                "comment_form": None,
+            },
+        )
